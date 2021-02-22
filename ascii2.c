d77201bcbc6fe7ed08a8e2e5b4f9ee1fe63cfc94
jodini2021daw2 <jodini2021daw2@gmail.com>
2021-02-22 19:47:16 +0100

    Commit 1 del projecte ascii2 a la branca ascii2_ampliada

--- ascii2.c
+++ ascii2.c
@@ -10,7 +10,8 @@ int main() {
 	printf("CAR  HEX  DEC\n");
 	n='Z'-'A';
 	for (i=n;i>=0;i--) {
-		c='A'+i
+		c='A'+i;
+		printf("%c	%x	%d\n", c+32, c+32, c+32);
 		printf("%c    %x   %d\n",c,c,c);
 	}
 	return(0);
