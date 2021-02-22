eff0a9666646ffeb5c8ada08f32bb7cc985ac9e3
jodini2021daw2 <jodini2021daw2@gmail.com>
2021-02-22 19:44:44 +0100

    Commit 1 del projecte ascii2

---
+++ ascii2.c
@@ -0,0 +1,17 @@
+//Autor: jordidiaznieto
+//Data: 22-02-21
+#include <stdio.h>
+
+int main() {
+	char c;
+	int n,i;
+		
+	printf("TAULA ASCII MAJÃšSCULES 07-02-2020\n");
+	printf("CAR  HEX  DEC\n");
+	n='Z'-'A';
+	for (i=n;i>=0;i--) {
+		c='A'+i
+		printf("%c    %x   %d\n",c,c,c);
+	}
+	return(0);
+}
